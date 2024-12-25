177cfc616523081c8ba2a39ad7c86ad308a3ff3f
Tobias Lundgren <tobias@lundgren.design>
2024-12-25 03:16:52 +0100

    Re-add the breaking news section

--- src/components/layouts/home-layout.tsx
+++ src/components/layouts/home-layout.tsx
@@ -3,10 +3,12 @@ import { RecentIPOsSection } from '@/components/home/sections/recent-ipos-sectio
 import { HeroSection } from '@/components/home/sections/hero';
 import { FeaturedIPO } from '@/components/home/sections/featured-ipo';
 import { SecondarySections } from '@/components/home/secondary-sections';
+import { BreakingNews } from '../home/sections/breaking-news';
 
 export function HomeLayout() {
   return (
     <main className="flex min-h-screen flex-col">
+      <BreakingNews />
       <NewsSection />
       <RecentIPOsSection />
       <HeroSection />
